ed5895f27b16ebb806bf550c69e8bb864a7e042b
Jordan Russel <rudzinski.a@gmail.com>
2021-02-06 19:57:46 +0100

    hamburger menu

--- development/app.581aa3f0.js
+++ development/app.581aa3f0.js
@@ -9563,7 +9563,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39839" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40943" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
